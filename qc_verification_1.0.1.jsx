56e008593ab863b6cc4921984da6ae451b170515
Austin Hixson <amh2659@LIB-PCLAS060.austin.utexas.edu>
2015-04-03 14:39:03 -0500

    Version 1.0.2
    //MODIFIED(1.0.2):New--> Shortened the message when resolution is not
    300DPI. I have commented out the message for when the resolution is
    correct, so that the user is only prompted when the resolution is not
    400DPI. I've left the code so that it can be used if ever needed.

--- qc_verification_1.0.1.jsx
+++ qc_verification_1.0.1.jsx
@@ -1,13 +1,13 @@
-#target photoshop
+﻿#target photoshop
 //Created for use by UT Libraries Preservation and Digitization Services
 //Created by Austin M. Hixson(austin@austin.utexas.edu) 2015-april-2
 //
-//Version 1.0.0 austin@austin.utexas.edu_2015-april-2
-//Version 1.0.1 austin@austin.utexas.edu_2015-april-2
+//Version 1.0.0 austin@austin.utexas.edu 2015-april-2
+//Version 1.0.2 austin@austin.utexas.edu 2015-april-3
 //
-//CHANGED(1.0.0): PREVIOUS->creates a confirmation prompt, which is used in the Script Events Manager. Only saves when OK button is clicked. austin@austin.utexas.edu_2015-april-2
-//
-//MODIFIED(1.0.1):New->Changed message to be based on a condition of the resolution being 400. If the resolution is the department standard of 400DPI, then the prompt will provide the resolution,  and height,and width in pixels and inches. If the resolution is not 400DPI, the prompt will tell the user what their current resolution is, and tell them they need to correct this image. austin@austin.utexas.edu_2015-april-2
+//CHANGED(1.0.0): PREVIOUS->creates a confirmation prompt, which is used in the Script Events Manager. Only saves when OK button is clicked. austin@austin.utexas.edu 2015-april-2
+//MODIFIED(1.0.1):New->Changed message to be based on a condition of the resolution being 400. If the resolution is the department standard of 400DPI, then the prompt will provide the resolution,  and height,and width in pixels and inches. If the resolution is not 400DPI, the prompt will tell the user what their current resolution is, and tell them they need to correct this image. austin@austin.utexas.edu 2015-april-2
+//MODIFIED(1.0.2):New--> Shortened the message when resolution is not 300DPI. I have commented out the message for when the resolution is correct, so that the user is only prompted when the resolution is not 400DPI. I've left the code so that it can be used if ever needed.
 //
 //Future Version Goals:
 //Prevent image from saving in future version. 
@@ -31,11 +31,17 @@ preferences.rulerUnits = Units.INCHES;
 var heightIN = activeDocument.height;
 var widthIN = activeDocument.width;
 
+//assign verification message
+var verMessage = "Good Job!"+"\nYour current resolution is: "+resolution+"DPI"+"\nYour current height is:  "+heightIN+" ; "+heightPX+"\nYour current width is:  "+widthIN+" ; "+widthPX+"\n";
+
+//check return based on resolution
+
 //Script to verify resolution of image being saved. START
 if (resolution == 400) {
-    alert("Your current resolution is: "+resolution+"DPI"+"\nYour current height is:  "+heightIN+" ; "+heightPX+"\nYour current width is:  "+widthIN+" ; "+widthPX+"\n");
+//I have commented out the message for when the resolution is correct, so that the user is only prompted when the resolution is not 400DPI. I've left the code so that it can be used if ever needed.
+//   alert(verMessage);
         } 
     else {
-    alert("Your current resolution is: "+resolution+" Make sure to correct this image!");
+("Your current resolution is: "+resolution+"DPI"+"\n"+"\nMake sure to correct this image!");
     };
 //Script to verify resolution of image being saved. END
